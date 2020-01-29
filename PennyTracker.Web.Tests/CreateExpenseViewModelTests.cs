8cf844f49ac723daa0c4b46467ddc12e19c8e5ee
Dimitar Radenkov <dimitar.radenkov1@gmail.com>
2020-01-29 07:20:54 +0200

    UT-01: added test project

---
+++ PennyTracker.Web.Tests/CreateExpenseViewModelTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PennyTracker.Web.Tests
+{
+    [TestClass]
+    public class CreateExpenseViewModelTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
