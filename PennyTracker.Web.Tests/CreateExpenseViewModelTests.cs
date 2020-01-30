290076711e87ec15233c4816613589b2d723aab4
Dimitar Radenkov <dimitar.radenkov1@gmail.com>
2020-01-30 18:43:22 +0200

    PT-01: added unit tests

--- PennyTracker.Web.Tests/CreateExpenseViewModelTests.cs
+++ PennyTracker.Web.Tests/CreateExpenseViewModelTests.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using Moq;
@@ -11,8 +12,24 @@ namespace PennyTracker.Web.Tests
     [TestClass]
     public class CreateExpenseViewModelTests
     {
+        private Expense expense;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.expense = new Expense
+            {
+                Id = 1,
+                Amount = 100,
+                Category = Category.Auto,
+                CreationDate = DateTime.UtcNow,
+                SpentDate = DateTime.UtcNow,
+                Description = "Petrol"
+            };
+        }
+
         [TestMethod]
-        public void WhenInitialzed_ShouldModelBeInstatiated()
+        public void OnInitialzed_WhenCreateNew_ShouldModelBeInstatiated()
         {
             //arrange
             var expenseServiceMock = new Mock<IExpenseService>();
@@ -27,7 +44,28 @@ namespace PennyTracker.Web.Tests
         }
 
         [TestMethod]
-        public void OnButtonSave_WhenInputIsCorrect_ShouldAddToExpenseService()
+        public void OnInitialzed_WhenEdit_ShouldModelBeInstatiated()
+        {
+            //arrange
+            var expenseServiceMock = new Mock<IExpenseService>();
+            expenseServiceMock.Setup(x => x.Get(It.IsAny<int>())).Returns(this.expense);
+
+            var dialogServiceMock = new Mock<IDialogService>();
+
+            var vm = new CreateExpenseViewModel(dialogServiceMock.Object, expenseServiceMock.Object);
+            vm.OnInitialized(this.expense.Id);
+
+            //assert
+            Assert.IsNotNull(vm.Model);
+            Assert.AreEqual(this.expense.Id, vm.Model.Id);
+            Assert.AreEqual(this.expense.Category, vm.Model.Category);
+            Assert.AreEqual(this.expense.CreationDate, vm.Model.CreationDate);
+            Assert.AreEqual(this.expense.SpentDate, vm.Model.SpentDate);
+            Assert.AreEqual(this.expense.Description, vm.Model.Description);
+        }
+
+        [TestMethod]
+        public void OnButtonSave_WhenCreateNew_ShouldCallAddExpense()
         {
             //arrange
             var expenseServiceMock = new Mock<IExpenseService>();
@@ -44,6 +82,28 @@ namespace PennyTracker.Web.Tests
 
             //assert
             expenseServiceMock.Verify(x => x.Add(vm.Model));
+            dialogServiceMock.Verify(x => x.Close(true));
+        }
+
+        [TestMethod]
+        public void OnButtonSave_WhenEdit_ShouldCallUpdateExpense()
+        {
+            //arrange
+            var expenseServiceMock = new Mock<IExpenseService>();
+            expenseServiceMock.Setup(x => x.Get(It.IsAny<int>())).Returns(this.expense);
+            expenseServiceMock.Setup(x => x.Add(It.IsAny<Expense>()));
+
+            var dialogServiceMock = new Mock<IDialogService>();
+            dialogServiceMock.Setup(x => x.Close(It.IsAny<bool>()));
+
+            var vm = new CreateExpenseViewModel(dialogServiceMock.Object, expenseServiceMock.Object);
+            vm.OnInitialized(this.expense.Id);
+
+            //act
+            vm.OnButtonSaveClicked();
+
+            //assert
+            expenseServiceMock.Verify(x => x.Update(vm.Model.Id, vm.Model));
             dialogServiceMock.Verify(x => x.Close(true));
         }
 
