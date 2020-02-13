ddbfee141195ec0f4cfd5d2274543ca6876ac863
Dimitar Radenkov <dimitar.radenkov1@gmail.com>
2020-02-13 06:13:45 +0200

    PT-04: added ExpensesController with DbContext

---
+++ PennyTracker.Api/Controllers/ExpensesController.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using PennyTracker.Api.Data;
+using PennyTracker.Shared.Models;
+
+namespace PennyTracker.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpensesController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public ExpensesController(ApplicationDbContext context)
+        {
+            dbContext = context;
+        }
+
+        // GET: api/Expenses
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Expense>>> GetExpenses()
+        {
+            return await dbContext.Expenses.ToListAsync();
+        }
+
+        // GET: api/Expenses/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Expense>> GetExpense(int id)
+        {
+            var expense = await dbContext.Expenses.FindAsync(id);
+
+            if (expense == null)
+            {
+                return NotFound();
+            }
+
+            return expense;
+        }
+
+        // PUT: api/Expenses/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutExpense(int id, Expense expense)
+        {
+            if (id != expense.Id)
+            {
+                return BadRequest();
+            }
+
+            dbContext.Entry(expense).State = EntityState.Modified;
+
+            try
+            {
+                await dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ExpenseExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Expenses
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Expense>> PostExpense(Expense expense)
+        {
+            dbContext.Expenses.Add(expense);
+            await dbContext.SaveChangesAsync();
+
+            return CreatedAtAction("GetExpense", new { id = expense.Id }, expense);
+        }
+
+        // DELETE: api/Expenses/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Expense>> DeleteExpense(int id)
+        {
+            var expense = await dbContext.Expenses.FindAsync(id);
+            if (expense == null)
+            {
+                return NotFound();
+            }
+
+            dbContext.Expenses.Remove(expense);
+            await dbContext.SaveChangesAsync();
+
+            return expense;
+        }
+
+        private bool ExpenseExists(int id)
+        {
+            return dbContext.Expenses.Any(e => e.Id == id);
+        }
+    }
+}
