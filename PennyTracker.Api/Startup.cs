d14f95f5e2a78f24de057bc94de5c7e66349826e
mitko100 <dimitar.radenkov1@gmail.com>
2020-02-20 15:37:52 +0200

    PT-14: added Repository with DI

--- PennyTracker.Api/Startup.cs
+++ PennyTracker.Api/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 
 using PennyTracker.Api.Data;
+using PennyTracker.Api.Repository;
 
 namespace PennyTracker.Api
 {
@@ -29,6 +30,8 @@ namespace PennyTracker.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddScoped<IExpenseRepository, ExpenseRepository>();
 
             services
                 .AddAuthentication(x =>
