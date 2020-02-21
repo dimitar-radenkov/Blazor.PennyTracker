d0900b9d0d1c14b8136c69d9747f5305a8592dcf
mitko100 <dimitar.radenkov1@gmail.com>
2020-02-21 18:11:53 +0200

    PT-16: added database seeding

--- PennyTracker.Api/ApplicationBuilderExtensions.cs
+++ PennyTracker.Api/ApplicationBuilderExtensions.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 using PennyTracker.Api.Data;
+using PennyTracker.Shared.Models;
 
 namespace PennyTracker.Api
 {
@@ -21,10 +22,9 @@ namespace PennyTracker.Api
                 {
                     var context = services.GetRequiredService<ApplicationDbContext>();
 
-                    //context.Database.EnsureDeleted();
+                    context.Database.EnsureDeleted();
                     context.Database.Migrate();
                     context.Database.EnsureCreated();
-
                 }
                 catch (Exception ex)
                 {
@@ -33,5 +33,41 @@ namespace PennyTracker.Api
                 }
             }
         }
+
+        public static void SeedDatabase(this IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+                    context.Database.EnsureCreated();
+
+                    context.Expenses.Add(new Expense { Description = "diesel", Category = Category.Auto, Amount = 150M, CreationDate = DateTime.UtcNow, SpentDate = DateTime.UtcNow });
+                    context.Expenses.Add(new Expense { Description = "diesel", Category = Category.Auto, Amount = 120M, CreationDate = DateTime.UtcNow.AddDays(-1), SpentDate = DateTime.UtcNow.AddDays(-1) });
+                    context.Expenses.Add(new Expense { Description = "diesel", Category = Category.Auto, Amount = 122M, CreationDate = DateTime.UtcNow.AddDays(-4), SpentDate = DateTime.UtcNow.AddDays(-4) });
+                    context.Expenses.Add(new Expense { Description = "food", Category = Category.Grocery, Amount = 350M, CreationDate = DateTime.UtcNow, SpentDate = DateTime.UtcNow });
+                    context.Expenses.Add(new Expense { Description = "lunch", Category = Category.EatingOut, Amount = 25, CreationDate = DateTime.UtcNow, SpentDate = DateTime.UtcNow });
+                    context.Expenses.Add(new Expense { Description = "lunch", Category = Category.EatingOut, Amount = 22, CreationDate = DateTime.UtcNow.AddDays(-1), SpentDate = DateTime.UtcNow.AddDays(-1) });
+                    context.Expenses.Add(new Expense { Description = "lunch", Category = Category.EatingOut, Amount = 14, CreationDate = DateTime.UtcNow.AddDays(-2), SpentDate = DateTime.UtcNow.AddDays(-2) });
+                    context.Expenses.Add(new Expense { Description = "lunch", Category = Category.EatingOut, Amount = 6, CreationDate = DateTime.UtcNow.AddDays(-3), SpentDate = DateTime.UtcNow.AddDays(-3) });
+                    context.Expenses.Add(new Expense { Description = "lunch", Category = Category.EatingOut, Amount = 9, CreationDate = DateTime.UtcNow.AddDays(-4), SpentDate = DateTime.UtcNow.AddDays(-4) });
+                    context.Expenses.Add(new Expense { Description = "lunch", Category = Category.EatingOut, Amount = 10, CreationDate = DateTime.UtcNow.AddDays(-5), SpentDate = DateTime.UtcNow.AddDays(-5) });
+                    context.Expenses.Add(new Expense { Description = "pharmacy", Category = Category.Healthcare, Amount = 150M, CreationDate = DateTime.UtcNow, SpentDate = DateTime.UtcNow });
+                    context.Expenses.Add(new Expense { Description = "mortgage", Category = Category.Loans, Amount = 750, CreationDate = DateTime.UtcNow, SpentDate = DateTime.UtcNow });
+
+                    context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+        }
+
+
     }
 }
