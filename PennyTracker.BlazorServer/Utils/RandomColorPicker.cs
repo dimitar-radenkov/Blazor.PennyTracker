237a5d10355aca9a17ce013b50924e3bcb7ecc5a
Dimitar Radenkov <dimitar.radenkov1@gmail.com>
2020-03-01 09:20:57 +0200

    PT-17: added report for expenses by category

---
+++ PennyTracker.BlazorServer/Utils/RandomColorPicker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace PennyTracker.BlazorServer.Utils
+{
+    public static class RandomKnownColorPicker
+    {
+        private static Random randomGenerator;
+        private static List<KnownColor> knownColors;
+        static RandomKnownColorPicker()
+        {
+            randomGenerator = new Random();
+            knownColors = Enum.GetValues(typeof(KnownColor)).Cast<KnownColor>().ToList();
+        }
+
+        public static KnownColor Get() => knownColors[randomGenerator.Next(knownColors.Count)];
+    }
+}
