491e7914e1dd5a5fca48a5fbfc2c4143466190cf
Dimitar Radenkov <dimitar.radenkov1@gmail.com>
2020-04-05 09:04:25 +0300

    testing yaml file change

--- PennyTracker.BlazorServer/Program.cs
+++ PennyTracker.BlazorServer/Program.cs
@@ -7,7 +7,7 @@ namespace PennyTracker.BlazorServer
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run(); 
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
